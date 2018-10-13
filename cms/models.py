faf7ce1179b1e0c9eb050db649b5f37d32fda504
Zorro05 <qazwsxedc1q>
2018-10-13 22:02:29 +0300

    fix text_field

--- cms/models.py
+++ cms/models.py
@@ -1,8 +1,8 @@
-from ckeditor.fields import RichTextField
+
 from django.db import models
 
 # Create your models here.
 class Text(models.Model):
     title = models.CharField(max_length=200)
-    content = RichTextField()
+    content = models.TextField()
     number = models.IntegerField(blank=True , null=True)
